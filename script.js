42fd08570a5880f9266edfc113ed5c57c39a21f4
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-03-02 23:08:17 +0000

    appended date to UI using moment JS

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  // Get today using Moment JS
+  const today = moment().format('DD MMMM YYYY');
+  console.log(today);
+  // Append date to DOM
+  $('#currentDay').append(`${today}`);
+});
