2b1f8814811d731a7277e220c001c442feb1c97e
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-03-05 17:33:13 +0000

    looped through timeblocks & colour coded - amended html/css

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 $(document).ready(function () {
-  // Get today using Moment JS
+  // Get today date using Moment JS
   const today = moment().format('DD MMMM YYYY');
-  console.log(today);
+
   // Append date to DOM
   $('#currentDay').append(`${today}`);
+
+  // Get hour using Moment
+  const thisHour = moment().hours();
+
+  // Loop through each timeblock & compare to this hour
+  $('.timeblock[data-hour]').each(function () {
+    const datahour = $(this).data('hour');
+    const hour = moment(datahour, 'HH:mm').hours();
+    if (hour < thisHour) {
+      $(this).addClass('past');
+    } else if (hour == thisHour) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+  });
 });
